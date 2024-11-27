5deab39db3f0f6b3a65ea2024be3901c735c5563
davidxv15 <davidxvaz@gmail.com>
2024-11-27 02:59:54 -0700

    testMongoConnect.js

---
+++ testMongoConnect.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb+srv://davidxvaz:UzJsu7NF3X3hntYU@clusterweather.9n4b9ot.mongodb.net/weatherApp?retryWrites=true&w=majority";
+
+async function connect() {
+    try {
+        const client = new MongoClient(uri);
+        await client.connect();
+        console.log("Connected successfully to MongoDB");
+        await client.close();
+    } catch (error) {
+        console.error("Connection failed:", error);
+    }
+}
+
+connect();
